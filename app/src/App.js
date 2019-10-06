99df6b5c198e01cde54832a49cefc073c93f2e33
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2019-10-06 20:01:55 +0300

    Parse alko products from csv. List products.

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,51 @@
 import React from 'react';
 import './App.css';
+import alko from './data/alko';
 
 function App() {
   return (
-    <div className="App">
-      Whisky
-    </div>
+      <table className="table">
+        <thead>
+        <tr>
+          <th>
+            Name
+          </th>
+          <th>
+            Abv
+          </th>
+          <th>
+            Price
+          </th>
+          <th>
+            Price if 40 %
+          </th>
+          <th>
+            Country
+          </th>
+        </tr>
+        </thead>
+        <tbody>
+        {alko.map(product =>
+            <tr key={product.id}>
+              <td>
+                {product.name}
+              </td>
+              <td>
+                {product.abv} %
+              </td>
+              <td>
+                {product.price.toFixed(2)} €
+              </td>
+              <td>
+                {(40 / product.abv * product.price).toFixed(2)} €
+              </td>
+              <td>
+                {product.country}
+              </td>
+            </tr>
+        )}
+        </tbody>
+      </table>
   );
 }
 
--- server/alko-parser.js
+++ server/alko-parser.js
@@ -2,4 +2,35 @@
  * Parse data from csv created from xlsx-file available in https://www.alko.fi/valikoimat-ja-hinnasto/hinnasto
  **/
 
-console.log('test');
+const readline = require('readline');
+const fs = require('fs');
+const results = [];
+
+const filePath = process.argv[2];
+
+const readInterface = readline.createInterface({
+  input: fs.createReadStream(filePath),
+});
+
+const separator = ';';
+let lineNumber = 1;
+readInterface.on('line', function(line) {
+  if (lineNumber > 4) {
+    let values = line.split(separator);
+    if (values[8] === 'viskit') {
+      results.push({
+        id: values[0],
+        name: values[1],
+        producer: values[2],
+        size: parseFloat(values[3].replace(' l', '').replace(',', '.')),
+        price: parseFloat(values[4]),
+        country: values[11],
+        notes: values[17],
+        abv: parseFloat(values[20]),
+      });
+    }
+  }
+  lineNumber++;
+}).on('close', () => {
+  fs.writeFileSync('alko.json', JSON.stringify(results));
+});
