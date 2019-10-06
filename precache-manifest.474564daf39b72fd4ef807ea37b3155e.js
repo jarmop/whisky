4fbe6ac145e03935013537b23c24d17885f58291
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2019-10-06 20:08:53 +0300

    Updates

---
+++ precache-manifest.474564daf39b72fd4ef807ea37b3155e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4602da595e536bfeff677cd31e32787",
+    "url": "/whisky/index.html"
+  },
+  {
+    "revision": "2a9ebcec76000f794481",
+    "url": "/whisky/static/css/main.e9eb798e.chunk.css"
+  },
+  {
+    "revision": "5d3c1e4d8bfe70eb8b1a",
+    "url": "/whisky/static/js/2.6dc43d8d.chunk.js"
+  },
+  {
+    "revision": "2a9ebcec76000f794481",
+    "url": "/whisky/static/js/main.2827bc88.chunk.js"
+  },
+  {
+    "revision": "a816385b452cb5ad96e4",
+    "url": "/whisky/static/js/runtime-main.75e8beb1.js"
+  }
+]);
