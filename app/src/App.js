f02735172ec72c917a2adb2fbadd53ef8c70be0b
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2019-10-26 17:11:49 +0300

    Filter products.

--- app/src/App.js
+++ app/src/App.js
@@ -1,21 +1,57 @@
 import React from 'react';
 import './App.css';
-import alko from './data/alko';
+import allProducts from './data/alko';
 import AlkoList from './AlkoList';
 
 
-alko.sort((a, b) => {
-  if (a.country === b.country) {
-    return a.relativePrice - b.relativePrice;
-  } else {
-    return 0;
-  }
-});
+const countryOptions = allProducts.reduce(
+    (accumulator, product) => {
+      if (!accumulator.includes(product.country)) {
+        accumulator.push(product.country);
+      }
+      return accumulator;
+    },
+    []
+);
 
 class App extends React.Component {
-  render(){
+  constructor() {
+    super();
+
+    this.state = {
+      selectedCountry: 'Skotlanti',
+    };
+  }
+
+  setSelectedCountry(country) {
+    this.setState({
+      selectedCountry: country,
+    })
+  }
+
+  render() {
+    let products = allProducts.filter(product => product.country === this.state.selectedCountry);
+    products.sort((a, b) => {
+      if (a.country === b.country) {
+        return a.relativePrice - b.relativePrice;
+      } else {
+        return 0;
+      }
+    });
+
     return (
-        <AlkoList products={alko}/>
+        <div>
+          <div className={{display: 'flex'}}>
+            <select onChange={(event) => this.setSelectedCountry(event.target.value)} value={this.state.selectedCountry}>
+              {countryOptions.map(country =>
+                  <option key={country} value={country}>{country}</option>
+              )}
+            </select>
+            {' tuotteita: ' + products.length}
+          </div>
+
+          <AlkoList products={products}/>
+        </div>
     );
   }
 }
