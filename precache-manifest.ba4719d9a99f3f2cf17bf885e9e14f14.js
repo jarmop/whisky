3a506e36344e422153702db8aeba2cdca1d08875
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2019-11-03 13:40:51 +0200

    Updates

--- precache-manifest.ba4719d9a99f3f2cf17bf885e9e14f14.js
+++ precache-manifest.ba4719d9a99f3f2cf17bf885e9e14f14.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7c9e8723c86a152882525fc32a051d44",
+    "revision": "1ca9c693e1421e40591a360f7ba11492",
     "url": "/whisky/index.html"
   },
   {
-    "revision": "5e50db6bec4c52318583",
-    "url": "/whisky/static/css/main.81198bbe.chunk.css"
+    "revision": "ac44c32843e49b2be53c",
+    "url": "/whisky/static/css/main.ae548812.chunk.css"
   },
   {
     "revision": "68895c9197904d10652f",
     "url": "/whisky/static/js/2.98889049.chunk.js"
   },
   {
-    "revision": "5e50db6bec4c52318583",
-    "url": "/whisky/static/js/main.f28d53f6.chunk.js"
+    "revision": "ac44c32843e49b2be53c",
+    "url": "/whisky/static/js/main.0c347468.chunk.js"
   },
   {
     "revision": "a816385b452cb5ad96e4",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/whisky/precache-manifest.ac0d94405b920a02ec7acc5c419fc077.js"
+  "/whisky/precache-manifest.ba4719d9a99f3f2cf17bf885e9e14f14.js"
 );
 
 self.addEventListener('message', (event) => {
