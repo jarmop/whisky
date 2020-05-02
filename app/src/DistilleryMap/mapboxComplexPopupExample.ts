a108e05316e8da607eda8e8e913699ce68b4662e
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2020-05-02 14:03:19 +0300

    Show distillery names on hover. Move complex popup example to another file.

---
+++ app/src/DistilleryMap/mapboxComplexPopupExample.ts
@@ -0,0 +1,78 @@
+import mapboxgl, { LngLatLike } from 'mapbox-gl';
+
+export default function addComplexPopup(map: mapboxgl.Map, popupCoordinates: number[]) {
+  map.on('load', () => {
+    map.addSource('places', {
+      type: 'geojson',
+      data: {
+        type: 'FeatureCollection',
+        features: [
+          {
+            type: 'Feature',
+            properties: {
+              description:
+                '<strong>Make it Mount Pleasant</strong><p>Make it Mount Pleasant is a handmade and vintage market and afternoon of live entertainment and kids activities. 12:00-6:00 p.m.</p>',
+              icon: 'theatre',
+            },
+            geometry: {
+              type: 'Point',
+              coordinates: popupCoordinates,
+            },
+          },
+        ],
+      },
+    });
+
+    map.addLayer({
+      id: 'places',
+      type: 'symbol',
+      source: 'places',
+      layout: {
+        'icon-image': '{icon}-15',
+        'icon-allow-overlap': true,
+      },
+    });
+
+    const popup = new mapboxgl.Popup({
+      closeButton: false,
+      closeOnClick: false,
+    });
+
+    map.on('mouseenter', 'places', (e) => {
+      if (
+        e.features === undefined
+        || e.features[0].geometry.type !== 'Point'
+        || !(e.features[0].properties && e.features[0].properties.hasOwnProperty('description'))
+      ) {
+        return;
+      }
+      // Change the cursor style as a UI indicator.
+      //   map.getCanvas().style.cursor = 'pointer';
+
+      const coordinates = e.features[0].geometry.coordinates.slice();
+      // const coordinates = geometry.coordinates.slice();
+      const { description } = e.features[0].properties;
+
+      // Ensure that if the map is zoomed out such that multiple
+      // copies of the feature are visible, the popup appears
+      // over the copy being pointed to.
+      while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+        coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+      }
+
+      const lngLat: LngLatLike = [coordinates[0], coordinates[1]];
+
+      // Populate the popup and set its coordinates
+      // based on the feature found.
+      popup
+        .setLngLat(lngLat)
+        .setHTML(description)
+        .addTo(map);
+
+      map.on('mouseleave', 'places', () => {
+        // map.getCanvas().style.cursor = '';
+        popup.remove();
+      });
+    });
+  });
+}
--- app/src/DistilleryMap/mapboxMap.ts
+++ app/src/DistilleryMap/mapboxMap.ts
@@ -1,8 +1,9 @@
-import mapboxgl, { LngLatLike } from 'mapbox-gl';
+import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 import { Coordinates } from 'types';
 import distilleries from 'data/distilleries';
+import addComplexPopup from './mapboxComplexPopupExample';
 
 const scotlandCenter: Coordinates = {
   lon: -4.5518,
@@ -18,87 +19,23 @@ export default function drawMap(containerId: string) {
     zoom: 6,
   });
 
-  distilleries.forEach((distillery) => new mapboxgl.Marker()
-    .setLngLat([distillery.coordinates.lon, distillery.coordinates.lat])
-    .addTo(map));
+  distilleries.forEach((distillery) => {
+    const marker = new mapboxgl.Marker()
+      .setLngLat([distillery.coordinates.lon, distillery.coordinates.lat])
+      .setPopup(
+        new mapboxgl.Popup({
+          // offset: 25,
+          closeButton: false,
+        }).setHTML(`<h3>${distillery.name}</h3>`),
+      )
+      .addTo(map);
 
-  map.on('load', () => {
-    map.addSource('places', {
-      type: 'geojson',
-      data: {
-        type: 'FeatureCollection',
-        features: [
-          {
-            type: 'Feature',
-            properties: {
-              description:
-                '<strong>Make it Mount Pleasant</strong><p>Make it Mount Pleasant is a handmade and vintage market and afternoon of live entertainment and kids activities. 12:00-6:00 p.m.</p>',
-              icon: 'theatre',
-            },
-            geometry: {
-              type: 'Point',
-              coordinates: [scotlandCenter.lon, scotlandCenter.lat],
-            },
-          },
-        ],
-      },
-    });
+    const markerDiv = marker.getElement();
+    markerDiv.addEventListener('mouseenter', () => marker.togglePopup());
+    markerDiv.addEventListener('mouseleave', () => marker.togglePopup());
 
-    map.addLayer({
-      id: 'places',
-      type: 'symbol',
-      source: 'places',
-      layout: {
-        'icon-image': '{icon}-15',
-        'icon-allow-overlap': true,
-      },
-    });
-
-    const popup = new mapboxgl.Popup({
-      closeButton: false,
-      closeOnClick: false,
-    });
-
-    map.on('mouseenter', 'places', (e) => {
-      if (
-        e.features === undefined
-        || e.features[0].geometry.type !== 'Point'
-        || !(e.features[0].properties && e.features[0].properties.hasOwnProperty('description'))
-      ) {
-        return;
-      }
-      // Change the cursor style as a UI indicator.
-      map.getCanvas().style.cursor = 'pointer';
-
-      const coordinates = e.features[0].geometry.coordinates.slice();
-      // const coordinates = geometry.coordinates.slice();
-      const { description } = e.features[0].properties;
-
-      // Ensure that if the map is zoomed out such that multiple
-      // copies of the feature are visible, the popup appears
-      // over the copy being pointed to.
-      while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-        coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-      }
-
-      const lngLat: LngLatLike = [coordinates[0], coordinates[1]];
-
-      // Populate the popup and set its coordinates
-      // based on the feature found.
-      popup
-        .setLngLat(lngLat)
-        .setHTML(description)
-        .addTo(map);
-
-      map.on('mouseleave', 'places', () => {
-        map.getCanvas().style.cursor = '';
-        popup.remove();
-      });
-    });
+    return marker;
   });
 
-  // new mapboxgl.Popup()
-  //   .setLngLat([scotlandCenter.lon, scotlandCenter.lat])
-  //   .setHTML('<h1>Null Island</h1>')
-  //   .addTo(map);
+  // addComplexPopup(map, [scotlandCenter.lon, scotlandCenter.lat]);
 }
