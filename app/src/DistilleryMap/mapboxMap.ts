3b69b04b9770df2304c5dc17367929501f229ca3
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2020-05-03 11:35:12 +0300

    Use custom element as a marker. Tweak styles. Increase default zoom in.

--- app/src/DistilleryMap/mapboxMap.ts
+++ app/src/DistilleryMap/mapboxMap.ts
@@ -1,7 +1,7 @@
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
-import { Coordinates } from 'types';
+import { Coordinates, Distillery } from 'types';
 import distilleries from 'data/distilleries';
 import addComplexPopup from './mapboxComplexPopupExample';
 
@@ -10,32 +10,40 @@ const scotlandCenter: Coordinates = {
   lat: 57.0042,
 };
 
+function createMarkerElement(): HTMLDivElement {
+  const element = document.createElement('div');
+  element.className = 'marker';
+
+  return element;
+}
+
+function addDistilleryToMap(map: mapboxgl.Map, distillery: Distillery) {
+  const marker = new mapboxgl.Marker(createMarkerElement())
+    .setLngLat([distillery.coordinates.lon, distillery.coordinates.lat])
+    .setPopup(
+      new mapboxgl.Popup({
+        closeButton: false,
+        className: 'popup',
+      }).setHTML(`<div class="popup-title">${distillery.name}</div>`),
+    )
+    .addTo(map);
+
+  // The popup opens on click by default, but other events can be used like this
+  // const markerElement = marker.getElement();
+  // markerElement.addEventListener('mouseenter', () => marker.togglePopup());
+  // markerElement.addEventListener('mouseleave', () => marker.togglePopup());
+}
+
 export default function drawMap(containerId: string) {
   mapboxgl.accessToken = 'pk.eyJ1IjoiamFybW9wIiwiYSI6ImNrOW8xM2lxNzAxbmYzbHJ5a200aXNjeGYifQ.HcClAUYB_z8izQnQS3TQ5A';
   const map = new mapboxgl.Map({
     container: containerId,
     style: 'mapbox://styles/mapbox/streets-v9',
     center: [scotlandCenter.lon, scotlandCenter.lat],
-    zoom: 6,
+    zoom: 6.5,
   });
 
-  distilleries.forEach((distillery) => {
-    const marker = new mapboxgl.Marker()
-      .setLngLat([distillery.coordinates.lon, distillery.coordinates.lat])
-      .setPopup(
-        new mapboxgl.Popup({
-          // offset: 25,
-          closeButton: false,
-        }).setHTML(`<h3>${distillery.name}</h3>`),
-      )
-      .addTo(map);
+  distilleries.forEach((distillery) => addDistilleryToMap(map, distillery));
 
-    const markerDiv = marker.getElement();
-    markerDiv.addEventListener('mouseenter', () => marker.togglePopup());
-    markerDiv.addEventListener('mouseleave', () => marker.togglePopup());
-
-    return marker;
-  });
-
-  // addComplexPopup(map, [scotlandCenter.lon, scotlandCenter.lat]);
+  addComplexPopup(map, [scotlandCenter.lon, scotlandCenter.lat]);
 }
